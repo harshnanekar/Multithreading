67fe051b8d493f196545ded3b7dc7278984ba523
harshnanekar <nanekarharshal2@gmail.com>
2025-03-19 00:21:32 +0530

    Added Execute method

--- src/main/java/spring/multithreading/AsyncMethod/AsyncController.java
+++ src/main/java/spring/multithreading/AsyncMethod/AsyncController.java
@@ -5,11 +5,11 @@ import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AsyncController {
 
   @Autowired
@@ -17,7 +17,7 @@ public class AsyncController {
 
   //Method for single thread
   @GetMapping("/single-thread")
-  public @ResponseBody ResponseEntity<?> testController1() {
+  public ResponseEntity<?> testController1() {
     try {
       
       //Calling without return type
@@ -40,7 +40,7 @@ public class AsyncController {
 
   //Method for parallel threads
   @GetMapping("/parallel-thread")
-  public @ResponseBody ResponseEntity<?> testController2() {
+  public ResponseEntity<?> testController2() {
     try {
       
       //Calling with return type
@@ -56,7 +56,7 @@ public class AsyncController {
 
    //Method for custom threads
    @GetMapping("/custom-thread")
-   public @ResponseBody ResponseEntity<?> testController3() {
+   public ResponseEntity<?> testController3() {
      try {
        
        asyncConfig.asyncThreadPool();
---
+++ src/main/java/spring/multithreading/ExecutorService/ExecutorServiceImpl.java
@@ -0,0 +1,76 @@
+package spring.multithreading.ExecutorService;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.multithreading.AsyncMethod.AsyncConfig;
+
+@RestController
+public class ExecutorServiceImpl {
+
+    // Method with Return Type
+    @GetMapping("/execute-service-test")
+    public ResponseEntity<?> executeTest() {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+            for (int i = 0; i < 6; i++) {
+                executorService.submit(() -> {
+                    try {
+                        Thread.sleep(10000);
+                        System.out.println("Thread executed successfully " + Thread.currentThread().getName());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        executorService.shutdownNow();
+                    }
+                });
+            }
+
+            executorService.shutdown();
+            if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+              System.out.println("Time Exceeded");
+              executorService.shutdownNow();
+            }
+
+            return new ResponseEntity<>("Thread Executed Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("failed To Execute Thread", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Method without Return Type
+    @GetMapping("/execute-service-test1")
+    public ResponseEntity<?> executeTest1() {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+            Future<String> result = executorService.submit(() -> {
+                try {
+                    Thread.sleep(10000);
+                    return "Thread completed";
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    executorService.shutdownNow();
+                    return "Thread falied";
+                }
+            });
+
+            result.get();
+            System.out.println(result.resultNow());
+            executorService.shutdown();
+
+            return new ResponseEntity<>("Thread Executed Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("failed To Execute Thread", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}

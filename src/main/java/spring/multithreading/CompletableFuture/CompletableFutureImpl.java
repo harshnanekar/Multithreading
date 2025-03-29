b3895e1361e06062758a6988e1adf43378ebe301
harshnanekar <nanekarharshal2@gmail.com>
2025-03-29 19:47:11 +0530

    Added completablefuture logic

--- src/main/java/spring/multithreading/CompletableFuture/CompletableFutureImpl.java
+++ src/main/java/spring/multithreading/CompletableFuture/CompletableFutureImpl.java
@@ -2,6 +2,8 @@ package spring.multithreading.CompletableFuture;
 
 import java.util.concurrent.CompletableFuture;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -114,6 +116,98 @@ public class CompletableFutureImpl {
             });
 
             res.thenAccept((result) -> System.out.println("Result of combine is " + result));
+
+            int sum2 = calculate(5, 5);
+            System.out.println("Calculation of sum 2 is " + sum2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Failed To Execute Thread ", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Thread Executed Successfully ", HttpStatus.OK);
+
+    }
+
+    @GetMapping("/completable-all-of")
+    public ResponseEntity<?> executeTask3() {
+
+        try {
+            int sum1 = calculate(3, 4);
+            System.out.println("Calculation of sum 1 is " + sum1);
+
+            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return calculate(6, 6);
+            });
+
+            CompletableFuture<Integer> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return calculate(6, 6);
+            });
+
+            CompletableFuture.allOf(completableFuture, completableFuture1)
+                    .exceptionally(ex -> {
+                        System.err.println("Exception occurred: " + ex.getMessage());
+                        return null;
+                    })
+                    .thenRun(() -> {
+                        System.out.println("Sum of first is " + completableFuture.join());
+                        System.out.println("Sum of second is " + completableFuture1.join());
+                    });
+
+            int sum2 = calculate(5, 5);
+            System.out.println("Calculation of sum 2 is " + sum2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Failed To Execute Thread ", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Thread Executed Successfully ", HttpStatus.OK);
+
+    }
+
+    @GetMapping("/completable-any-of")
+    public ResponseEntity<?> executeTask4() {
+
+        try {
+            int sum1 = calculate(3, 4);
+            System.out.println("Calculation of sum 1 is " + sum1);
+
+            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(5000);
+                    throw new RuntimeException("Error in task 1");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return calculate(6, 6);
+            });
+
+            CompletableFuture<Integer> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return calculate(6, 6);
+            });
+
+            CompletableFuture.anyOf(completableFuture, completableFuture1)
+                    .whenComplete((result, ex) -> {
+                        if (ex != null) {
+                            System.out.println("Error " + ex.getMessage());
+                        } else {
+                            System.out.println("Sum is " + result);
+                        }
+                    });
 
             int sum2 = calculate(5, 5);
             System.out.println("Calculation of sum 2 is " + sum2);
